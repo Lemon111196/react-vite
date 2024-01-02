1ed3845ac268ba486d4c638cf751bf6eb1203851
Ngoc Dinh <dinhtienngoc96@gmail.com>
2024-01-02 20:21:42 +0700

    update

--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -34,80 +34,82 @@ export default function Todo() {
                 id: Math.floor(Math.random() * 1000000000000),
                 [e.target?.name]: e.target?.value,
             })
-            //! Thêm vào 1 note
-            const createTodo = () => {
-                setFormData({
-                    id,
-                    todo: "",
-                    isEdit: false,
-                })
-                setTodo([...todo, formData]) //thêm todo vào để ghi nhớ giá trị đã thêm vào trc nó
-            }
-            //! Open modal
-            const [updateModal, setUpdateModal] = useState<boolean>(false);
-            const showModal = () => {
-                setUpdateModal(true);
-            }
-
-            //! Edit content
-            const closeUpdateDialog = () => {
-                setFormData({
-                    id,
-                    todo: "",
-                    isEdit: false,
-                });
-                setUpdateModal(false);
-            };
-
-            //! Delete todo
-            const [deleteData, setDeleteData] = useState<boolean>(false);
-            const deleteTodo () => {
-                setDeleteData(true);
-            }
-            return (
-                <TodoContainer>
-                    <h1>Todo List</h1>
-                    <div className="addItem">
-                        <TextField
-                            id="outlined-basic"
-                            size="small"
-                            label="Todo"
-                            variant="outlined"
-                            name="todo"
-                            value={formData.todo}
-                            onChange={onChange} />
-                        <Button variant="outlined" onClick={createTodo}>Add</Button>
-
-                    </div>
-                    <div className="listItem">
-                        {todo.map((item, index) => (
-                            <div className="singleTodo" key={index}>
-                                <div className="content">
-                                    <span className="todo-no">{index + 1}.</span>
-                                    <span>{item.todo}</span>
-                                </div>
-                                <div className="btnGroup">
-                                    <ModeEditOutlineOutlinedIcon onClick={showModal} className="edit"></ModeEditOutlineOutlinedIcon>
-                                    <DeleteOutlineOutlinedIcon className="delete"></DeleteOutlineOutlinedIcon>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                    <Dialog open={updateModal} title="Update List" >
-                        <TextFieldStyle
-                            name="todo"
-                            value={formData.todo}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e, true)}>
-
-                        </TextFieldStyle>
-                    </Dialog>
-                    <Dialog open={deleteData}
-                        title="Delete Todo"
-                        submitBtn="Delete"
-                        onCancel={closeUpdateDialog}
-                        onSubmit={deleteTodo}>
-                        <p>Are you sure to delete this?</p>
-                    </Dialog>
-                </TodoContainer>
-            )
         }
+        //! Thêm vào 1 note
+        const createTodo = () => {
+            setFormData({
+                id,
+                todo: "",
+                isEdit: false,
+            })
+            setTodo([...todo, formData]) //thêm todo vào để ghi nhớ giá trị đã thêm vào trc nó
+        }
+        //! Open modal
+        const [updateModal, setUpdateModal] = useState<boolean>(false);
+        const showModal = () => {
+            setUpdateModal(true);
+        }
+
+        //! Edit content
+        const closeUpdateDialog = () => {
+            setFormData({
+                id,
+                todo: "",
+                isEdit: false,
+            });
+            setUpdateModal(false);
+        };
+
+        //! Delete todo
+        const [deleteData, setDeleteData] = useState<boolean>(false);
+        const deleteTodo = () => {
+            setDeleteData(true);
+        }
+        return (
+            <TodoContainer>
+                <h1>Todo List</h1>
+                <div className="addItem">
+                    <TextField
+                        id="outlined-basic"
+                        size="small"
+                        label="Todo"
+                        variant="outlined"
+                        name="todo"
+                        value={formData.todo}
+                        onChange={onChange} />
+                    <Button variant="outlined" onClick={createTodo}>Add</Button>
+
+                </div>
+                <div className="listItem">
+                    {todo.map((item, index) => (
+                        <div className="singleTodo" key={index}>
+                            <div className="content">
+                                <span className="todo-no">{index + 1}.</span>
+                                <span>{item.todo}</span>
+                            </div>
+                            <div className="btnGroup">
+                                <ModeEditOutlineOutlinedIcon onClick={showModal} className="edit"></ModeEditOutlineOutlinedIcon>
+                                <DeleteOutlineOutlinedIcon className="delete"></DeleteOutlineOutlinedIcon>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <Dialog open={updateModal} title="Update List" >
+                    <TextFieldStyle
+                        name="todo"
+                        value={formData.todo}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e, true)}>
+
+                    </TextFieldStyle>
+                </Dialog>
+                <Dialog open={deleteData}
+                    title="Delete Todo"
+                    submitBtn="Delete"
+                    onCancel={closeUpdateDialog}
+                    onSubmit={deleteTodo}>
+                    <p>Are you sure to delete this?</p>
+                </Dialog>
+            </TodoContainer>
+        )
+    }
+}
